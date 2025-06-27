f202aa61471be703dfb7a65cd607dc591e010122
Gamerk786po <abdullahpper@gmail.com>
2025-06-27 12:39:53 +0500

    responsivness for anime-girl

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,22 @@
+import AnimeGirl from "./components/anime-girl/anime-girl";
 
+// The app function
 function App() {
+  // Returning
   return (
-    <>
-    <h1 className="text-blue-600">Hello dude</h1>
-    </>
-  )
+    // THe main div
+    <div
+      className="h-screen bg-center flex flex-col md:flex-row justify-start items-center"
+      style={{
+        backgroundImage: `url(/imgs/sparkle.png), linear-gradient(to bottom, #9B59B6, #BA55D3, #FFB6C1)`,
+        backgroundSize: "200px, cover", 
+        backgroundRepeat: "repeat, no-repeat",
+        backgroundPosition: "center, center",
+      }}
+    >
+      <AnimeGirl />
+    </div>
+  );
 }
 
-export default App
+export default App;
