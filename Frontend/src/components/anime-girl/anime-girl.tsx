05d31b4f6eecf01609b9e9a4bef759b30079fff1
Gamerk786po <abdullahpper@gmail.com>
2025-07-01 03:31:25 +0500

    change

--- Frontend/src/components/anime-girl/anime-girl.tsx
+++ Frontend/src/components/anime-girl/anime-girl.tsx
@@ -4,7 +4,7 @@ const AnimeGirl = () => {
         <>
         {/* The div containing the anime-girl png */}
         <div className="md:mx-10 lg:mx-16 2xl:mx-40">
-            <img src="/imgs/anime-girl.png" className=" h-[15rem] w-[10rem] md:h-[20rem] md:w-[13rem] lg:h-[30rem] lg:w-[20rem] 2xl:h-[50rem] 2xl:w-[34rem] mt-8" alt="Its a img for anime-girl"></img>
+            <img src="/imgs/anime-girl.png" className=" h-[15rem] w-[10rem] md:h-[20rem] md:w-[13rem] lg:h-[30rem] lg:w-[20rem] 2xl:h-[50rem] 2xl:w-[34rem] mt-8" alt="Its a img for anime-girl" loading="eager"></img>
         </div>
         </>
     )
